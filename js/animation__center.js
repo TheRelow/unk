fb13f1b47b4f0f485f992c1decd3893a159327d4
Egor Antropov <relow20@gmail.com>
2019-12-23 18:14:18 +0300

    first commit

---
+++ js/animation__center.js
@@ -0,0 +1,21 @@
+var animation__line1 = document.querySelector('#animation__line1');
+var animation__line2 = document.querySelector('#animation__line2');
+var animation__line3 = document.querySelector('#animation__line3');
+var item1 = document.querySelector('#item1');
+var item2 = document.querySelector('#item2');
+var item3 = document.querySelector('#item3');
+var item4 = document.querySelector('#item4');
+var leftBottom = document.querySelector('#left-bottom');
+var rightTop = document.querySelector('#right-top');
+
+window.onload = function() {
+	animation__line1.classList.add('content-loaded');
+	animation__line2.classList.add('content-loaded');
+	animation__line3.classList.add('content-loaded');
+	item1.classList.add('content-loaded');
+	item2.classList.add('content-loaded');
+	item3.classList.add('content-loaded');
+	item4.classList.add('content-loaded');
+	leftBottom.classList.add('content-loaded');
+	rightTop.classList.add('content-loaded');
+};
